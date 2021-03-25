375682836a6d39146712d104dc3916c279a613a4
malay3 <malay@goodhealthapp.com>
2021-03-25 11:40:49 +0530

    added package

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,48 @@
-var path = require('path');
+const path = require('path');
+
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve('lib'),
     filename: 'index.js',
-    libraryTarget: 'commonjs2'
+    libraryTarget: 'commonjs2',
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|bower_components|build)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env']
-          }
-        }
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader',
       },
       {
-      test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-    ]
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ],
+  },
+  resolve: {
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    }
   },
   externals: {
-    'react': 'commonjs react'
-}
-}
+    // Don't bundle react or react-dom      
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    }
+  }
+};
